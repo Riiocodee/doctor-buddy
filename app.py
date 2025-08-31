532dc456d63e9d2532309f050f32a352f90ee011
Riya Singh Bhadoria <jr.mpriya225@gmail.com>
2025-09-01 00:00:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-# Replace with your Java installation path if different
-os.environ["JAVA_HOME"] = r"C:\Program Files\Java\jdk-23.0.2"
-os.environ["PATH"] += os.pathsep + os.path.join(os.environ["JAVA_HOME"], "bin")
+
+# Point to your Java installation
+java_path = r"C:\Program Files\Java\jdk-23.0.2\bin"  # adjust if your path is different
+os.environ["JAVA_HOME"] = java_path
+os.environ["PATH"] = java_path + os.pathsep + os.environ["PATH"]
 import streamlit as st
 import pandas as pd
 import json
@@ -310,6 +312,7 @@ if st.session_state.page == "login" or not st.session_state.logged_in:
     registration_ui()
 elif st.session_state.page == "main":
     main_app_ui()
+
 
 
 
